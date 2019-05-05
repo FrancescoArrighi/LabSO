a1b2e344f04023bff7d3abd3feae77547890d19b
Wendy Barreto <005.wen@gmail.com>
2019-05-05 23:49:29 +0200

    Aggiornamento Header

--- comunicazione_base.c
+++ comunicazione_base.c
@@ -1,47 +1,10 @@
-#include <stdio.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
+//Header globale
+#include "global.h"
 
-
-//valori di verità
-#define TRUE 1
-#define FALSE 0
-
-//dispositivi
-#define CONTROLLER 0
-#define HUB 1
-#define TIMER 2
-#define BULB 3
-#define WINDOW 4
-#define FRIDGE 5
-
-//pipes
-#define READ 0
-#define WRITE 1
-
-//funzioni
-#define ADD 1
-
-struct msgbuf{
+typedef struct msgbuf{
     long msg_type;
     char msg_text[100];
-};
-
-typedef struct msgbuf msgbuf;
+} msgbuf;
 
 void padre(){
     key_t key;
@@ -91,6 +54,6 @@ int main() {
     else{
         figlio();
     }
-    
+
     return 0;
 }
