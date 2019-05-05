e6851b6de806fc1d6a7629bc5914e87e42573cdd
Wendy Barreto <005.wen@gmail.com>
2019-05-05 22:57:30 +0200

    Aggiunto Header
    Aggiunto Header con tutte le librerie e define

--- LabSO.c
+++ LabSO.c
@@ -1,40 +1,5 @@
-//Librerie che possiamo usare
-#include <stdio.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-
-//valori di verità
-#define TRUE 1
-#define FALSE 0
-
-//dispositivi
-#define CONTROLLER 0
-#define HUB 1
-#define TIMER 2
-#define BULB 3
-#define WINDOW 4
-#define FRIDGE 5
-
-//pipes
-#define READ 0
-#define WRITE 1
-
-//funzioni
-#define ADD 1
+//Header globale
+#include "global.h"
 
 //Struct
 typedef struct pair_int{
