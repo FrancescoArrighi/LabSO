33a1beefc5bc9978e6aa30f426fa3f35a71cee7d
Wendy Barreto <005.wen@gmail.com>
2019-05-13 00:57:09 +0200

    Revert "Useful aggiornata"
    This reverts commit 4ff7f62c8d054be39bc3aec04bb7d3f8f6fb7d08.

--- useful_fun.h
+++ useful_fun.h
@@ -4,6 +4,42 @@
 #ifndef USEFUL_FUN_H
 #define  USEFUL_FUN_H
 
+//Struct utili
+typedef struct pair_int{
+    int first;
+    int second;
+} pair_int;
+
+typedef struct device{
+    int id;
+    int fd[2];
+} device;
+
+typedef struct int_node {
+    int val;
+    struct int_node * next;
+} int_node;
+
+typedef struct int_list{
+    int_node * head;
+    int n;
+} int_list;
+
+typedef struct pair_int_device{
+    int integer;
+    device * val;
+} pair_int_device;
+
+typedef struct device_node{
+    device * val;
+    struct device_node * next;
+} device_node;
+
+typedef struct device_list{
+    device_node * head;
+    int n;
+} device_list;
+
 //Dichiarazioni Funzioni
 pair_int get_int(int n, int_list * list);
 int_list * create_int_list();
