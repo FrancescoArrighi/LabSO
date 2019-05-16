229190b2c41d6aa796f054479350a5015638550b
Zhugxz <32508354+Zhugxz@users.noreply.github.com>
2019-05-16 12:45:47 +0200

    Frigo -> fridge

--- fridge.h
+++ fridge.h
@@ -17,21 +17,21 @@ typedef struct frigo{
   int delay;
   int percentuale;
   char * nome;
-} Frigo;
+} fridge;
 
 void fridge(int id, int recupero);
-void send_prl_dati(char* prl, Frigo *frigo); //funzione che concatena i dati del frigo (quelli di info)
+void send_prl_dati(char* prl, fridge *frigo); //funzione che concatena i dati del frigo (quelli di info)
 char * plus_only_n (char * a);
 int controlla_validita(char ** str, int id);
 void header_risposta(char **msg, char *header); //funzione che prende header della richiesta (msg) e restituisce header della risposta (header)
-void apri_frigo(Frigo *frigo, time_t *t_start, int *allarme, int delay_recupero); // delay_recupero: indica il delay rimenente del frigo recuperato
-void chiudi_frigo(Frigo *frigo, time_t *t_start, int *allarme);
-void set_stato(int valore, Frigo *frigo, time_t *t_start, int *allarme); // setta stato del frigo al valore <valore> e inizializza un timer
-void set_interruttore(int valore, Frigo *frigo, time_t *t_start, int *allarme);
-void set_termostato(int valore, Frigo *frigo);
-void set_delay(int valore, Frigo *frigo);
-void set_perc(int valore, Frigo *frigo);
-void duplicate(int valore, Frigo *frigo, time_t t_start); //salva i dati del frigo e li invia al frigo con id = valore
+void apri_frigo(fridge *frigo, time_t *t_start, int *allarme, int delay_recupero); // delay_recupero: indica il delay rimenente del frigo recuperato
+void chiudi_frigo(fridge *frigo, time_t *t_start, int *allarme);
+void set_stato(int valore, fridge *frigo, time_t *t_start, int *allarme); // setta stato del frigo al valore <valore> e inizializza un timer
+void set_interruttore(int valore, fridge *frigo, time_t *t_start, int *allarme);
+void set_termostato(int valore, fridge *frigo);
+void set_delay(int valore, fridge *frigo);
+void set_perc(int valore, fridge *frigo);
+void duplicate(int valore, fridge *frigo, time_t t_start); //salva i dati del frigo e li invia al frigo con id = valore
 
 
 #endif
